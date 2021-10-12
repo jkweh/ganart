323940d70c73c8a3d3d1f7a6cd4f55d7e4b4366a
JK <jeffkois@pm.me>
2021-10-11 22:04:11 -0400

    Refactor

--- scrape-wikiart-by-genre.py
+++ scrape-wikiart-by-genre.py
@@ -1,99 +1,87 @@
-import urllib
-import re
 from bs4 import BeautifulSoup
-import time
+from concurrent.futures import as_completed, ThreadPoolExecutor
+import re
+import requests
 
 file_path = "art/wikiart"
 base_url = "https://www.wikiart.org"
+genres = ["pop-art", "minimalism", "contemporary", "kitsch"]
 
-genres = ['pop-art', 'minimalism', 'contemporary', 'kitsch']
 
-# iterate through all artists by last name alphabetically
-for g in genres:
-  for c in range(ord('a'), ord('z')+1):
-      artist_list_url = f'{base_url}/en/artists-by-art-movement/{g}/text-list'
+def make_request(url: str):
+    return requests.get(
+        url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0"}
+    ).content
 
-      genre_soup = BeautifulSoup(urllib.request.urlopen(artist_list_url), "lxml")
-      artist_list_main = genre_soup.find("main")
-      lis = artist_list_main.find_all("li")
 
-      # for each list element
-      for li in lis: 
-          born = 0
-          died = 0
+def scrape_genre(genre: str):
+    for _ in range(ord("a"), ord("z") + 1):  # Iterate through each artist in the genre, by last name.
+        artist_list_url = f"{base_url}/en/artists-by-art-movement/{genre}/text-list"
+        genre_soup = BeautifulSoup(make_request(artist_list_url), "lxml")
+        artist_list_main = genre_soup.find("main")
 
-          # get the date range
-          for line in li.text.splitlines():
-              if line.startswith(",") and "-" in line:
-                  parts = line.split('-')
-                  if len(parts) == 2:
-                      born = int(re.sub("[^0-9]", "",parts[0]))
-                      died = int(re.sub("[^0-9]", "",parts[1]))
+        for li in artist_list_main.find_all("li"):
+            born = died = 0
+            for line in li.text.splitlines():  # get the date range
+                if line.startswith(",") and "-" in line:
+                    parts = line.split("-")
+                    if len(parts) == 2:
+                        born = int(re.sub("[^0-9]", "", parts[0]))
+                        died = int(re.sub("[^0-9]", "", parts[1]))
 
-          # look for artists who may have created work that could be in public domain
-          if born>1850 and died>0 and (born<1900 or died<1950):
-              link = li.find("a")
-              artist = link.attrs["href"]
+            # look for artists who may have created work that could be in public domain
+            if born > 1850 and died > 0 and (born < 1900 or died < 1950):
+                artist = li.find("a").attrs["href"]
 
-  #             if artist == "/en/salvador-dali": # skip Dali
-  #                 continue
+                # if artist == "/en/salvador-dali": # skip Dali
+                #     continue
 
-              # get the artist's main page
-              artist_url = base_url + artist
-              artist_soup = BeautifulSoup(urllib.request.urlopen(artist_url), "lxml")
+                # get the artist's main page
+                artist_soup = BeautifulSoup(make_request(f"{base_url}{artist}"), "lxml")
+                # page_body = artist_soup.text.lower()
+                print(f"{artist}:{str(born)}-{str(died)}")
+                url = f"{base_url}{artist}/all-works/text-list"  # get the artist's web page for the artwork
+                artist_work_soup = BeautifulSoup(make_request(url), "lxml")
 
-              page_body = artist_soup.text.lower()
-              print(artist + " " + str(born) + " - " + str(died))
+                # get the main section
+                artist_main = artist_work_soup.find("main")
+                artist_name = artist.split("/")[2]
+                image_count = 0
+                for li in artist_main.find_all("li"):
+                    link = li.find("a")
+                    if not link:
+                        continue
 
-              # get the artist's web page for the artwork
-              url = base_url + artist + '/all-works/text-list'
-              artist_work_soup = BeautifulSoup(urllib.request.urlopen(url), "lxml")
+                    url = f"{base_url}{link.attrs['href']}"  # get the painting
+                    try:
+                        painting_soup = BeautifulSoup(make_request(url), "lxml")
+                    except:
+                        print("error retreiving page")
+                        continue
 
-              # get the main section
-              artist_main = artist_work_soup.find("main")
-              image_count = 0
-              artist_name = artist.split("/")[2]
+                    if "public domain" not in painting_soup.text.lower():  # check the copyright
+                        continue
 
-              # get the list of artwork
-              lis = artist_main.find_all("li")
+                    painting_genre = painting_soup.find("span", {"itemprop": "genre"})
+                    if genre in painting_genre.text.lower():
+                        # get the url
+                        og_image = painting_soup.find("meta", {"property": "og:image"})
+                        image_url = og_image["content"].split("!")[0]  # ignore the !Large.jpg at the end
+                        save_path = f"{file_path}/{artist_name}_{str(image_count)}.jpg"
+                        try:  # download the file
+                            print(f"downloading {image_url} to {save_path}")
+                            open(save_path, "wb").write(make_request(image_url))
+                            image_count += 1
+                        except Exception as e:
+                            print(f"failed to download {image_url}", e)
 
-              # for each list element
-              for li in lis:
-                  link = li.find("a")
 
-                  if link != None:
-                      painting = link.attrs["href"]
-
-                      # get the painting
-                      url = base_url + painting
-                      print(url)
-
-                      try:
-                          painting_soup = BeautifulSoup(urllib.request.urlopen(url), "lxml")
-
-                      except:
-                          print("error retreiving page")
-                          continue
-
-                      # check the copyright
-                      if "public domain" in painting_soup.text.lower():
-
-                          #check the genre
-                          genre = painting_soup.find("span", {"itemprop":"genre"})
-                          if genre != None and genre.text == "abstract":
-
-                              # get the url
-                              og_image = painting_soup.find("meta", {"property":"og:image"})
-                              image_url = og_image["content"].split("!")[0] # ignore the !Large.jpg at the end
-                              print(image_url)
-
-                              save_path = f'{file_path}/{artist_name}_{str(image_count)}.jpg'
-
-                              #download the file
-                              try:
-                                  print("downloading to " + save_path)
-                                  time.sleep(0.2)  # try not to get a 403                    
-                                  urllib.request.urlretrieve(image_url, save_path)
-                                  image_count = image_count + 1
-                              except Exception as e:
-                                  print("failed downloading " + image_url, e) 
+if __name__ == "__main__":
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        futures = {executor.submit(scrape_genre, genre): genre for genre in genres}
+        for future in as_completed(futures):
+            genre = futures[future]
+            try:
+                data = future.result()
+            except Exception as exc:
+                print(f"{genre} scraper generated an exception: {exc}")
