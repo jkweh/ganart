a2b26875e1ddb11ee69774475ded2d97f2a2a068
JK <jeffkois@pm.me>
2021-11-30 21:29:20 -0500

    Take path as an argument

--- post_process_images.py
+++ post_process_images.py
@@ -1,33 +1,35 @@
-#!/usr/bin/env python3
+import os
+from sys import argv
 
 import numpy as np
 from PIL import Image
 from ISR.models import RDN, RRDN
 
+path = argv[1]
+if not os.path.isdir(path):
+    raise RuntimeError("Given path does not exist")
+
 # Import the image
-img = Image.open("input.png")
+for f in os.listdir(path):
+    img = Image.open(f"{path}/{f}")
+    model = RRDN(weights="gans")  # Load the GAN model that will perform a 4x resize
+    npimg = np.array(img)  # Convert to numpy image
 
-# Load the GAN model that will perform a 4x resize
-model = RRDN(weights="gans")
+    add_noise = False
+    if add_noise:  # Add some noise
+        row, col, ch = npimg.shape
+        mean = 0
+        var = 0.1
+        sigma = var ** 0.5
+        gauss = np.random.normal(mean, sigma, (row, col, ch)) * 24
 
-# Convert to numpy image
-npimg = np.array(img)
-row, col, ch = npimg.shape
+        # Reshape and clip the pixel values
+        gauss = gauss.reshape(row, col, ch)
+        noisy = np.clip(npimg + gauss, 0, 255).astype("uint8")
+        noisy_image = Image.fromarray(noisy)
+        npimg = np.array(noisy_image)
 
-# Add some noise
-mean = 0
-var = 0.1
-sigma = var ** 0.5
-gauss = np.random.normal(mean, sigma, (row, col, ch)) * 24
-
-# Reshape and clip the pixel values
-gauss = gauss.reshape(row, col, ch)
-noisy = np.clip(npimg + gauss, 0, 255).astype("uint8")
-noisy_image = Image.fromarray(noisy)
-
-# Do the resize
-big_np = model.predict(np.array(noisy_image))
-
-# Save the image
-big_img = Image.fromarray(big_np)
-big_img.save("big.jpg")
+    # Resize and save.
+    big_img = Image.fromarray(model.predict(npimg))
+    filename_no_ext = os.path.splitext(f)[0]
+    big_img.save(f"{path}/{filename_no_ext}-post.jpg")
